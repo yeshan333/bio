f66ae9b21581c4aeb003ab80695e6806ac34631f
yeshan333 <yeshan333.ye@gmail.com>
2024-04-21 17:44:39 +0800

    chore: fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://yeshan333.github.io/bio',
+	site: 'https://yeshan333.github.io',
 	integrations: [
 		starlight({
 			title: 'ShanSan',
